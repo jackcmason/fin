57341d429897b391a759a59249ed1bde4a086dea
Jack Mason <jack.mason@posteo.net>
2019-01-15 13:24:54 +1000

    reimplemented getDataList() with submethods

--- csvtodata.py
+++ csvtodata.py
@@ -19,63 +19,82 @@ def csvToList(csvfile):
     return collect
 
 def getCode(row):
-    return row[0]
+    return row[CODE]
 
 def getDate(row):
-    return row[1]
+    return row[DATE]
 
 def getOpen(row):
-    return float(row[2])
+    return float(row[OPEN])
 
 def getHigh(row):
-    return float(row[3])
+    return float(row[HIGH])
 
 def getLow(row):
-    return float(row[4])
+    return float(row[LOW])
 
 def getClose(row):
-    return float(row[5])
+    return float(row[CLOSE])
 
 def getVolume(row):
-    return int(row[6])
+    return int(row[VOLUME])
 
 def getValue(row):
-    return float(row[7])
+    return float(row[VALUE])
 
 def getTrades(row):
-    return int(row[8])
+    return int(row[TRADES])
 
 def getDataList(data, index):
     collect = []
     for row in data:
-        element = row[index]
+        if index == CODE:
+            element = getCode(row)
+        elif index == DATE:
+            element = getDate(row)
+        elif index == OPEN:
+            element = getOpen(row)
+        elif index == HIGH:
+            element = getHigh(row)
+        elif index == LOW:
+            element = getLow(row)
+        elif index == CLOSE:
+            element = getClose(row)
+        elif index == VOLUME:
+            element = getVolume(row)
+        elif index == VALUE:
+            element = getValue(row)
+        elif index == TRADES:
+            element = getTrades(row)
+        else:
+            raise ValueError('index is out of range for data')
         collect.append(element)
     return collect
 
 def getCodeList(data):
-    return getDataList(data, 0)
+    return getDataList(data, CODE)
 
 def getDateList(data):
-    return getDataList(data, 1)
+    return getDataList(data, DATE)
 
 def getOpenList(data):
-    return getDataList(data, 2)
+    return getDataList(data, OPEN)
 
 def getHighList(data):
-    return getDataList(data, 3)
+    return getDataList(data, HIGH)
 
 def getLowList(data):
-    return getDataList(data, 4)
+    return getDataList(data, LOW)
 
 def getCloseList(data):
-    return getDataList(data, 5)
+    return getDataList(data, CLOSE)
 
 def getVolumeList(data):
-    return getDataList(data, 6)
+    return getDataList(data, VOLUME)
 
 def getValueList(data):
-    return getDataList(data, 7)
+    return getDataList(data, VALUE)
 
 def getTradesList(data):
-    return getDataList(data, 8)
+    return getDataList(data, TRADES)
 
