cc6e4607d07f6fd3ea6c3e0405ffc63ff36e2b3a
Jack Mason <jack.mason@posteo.net>
2019-01-16 13:07:40 +1000

    addded tradeday class

---
+++ tradeday.py
@@ -0,0 +1,14 @@
+class Tradeday:
+    def init(self, code, date, openprice, high, low, close, volume, value, trades):
+        self.code = code
+        self.date = date
+        self.open = openprice
+        self.high = high
+        self.low = low
+        self.close = close
+        self.volume = volume
+        self.value = value
+        self.trades = trades
+    def __str__():
+        return "Code:{}\nDate:{}\nOpen:{}\nHigh:{}\nLow:{}\nClose:{}\nVolume:{}\nValue:{}\nTrades:{}".format(
+            self.code, self.date, self.open, self.high, self.low, self.close, self.volume, self.value, self.trades)
