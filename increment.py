ce3206539c8f5ec81575d8b190ef6a055f59f5c6
Jack Mason <jack.mason@posteo.net>
2019-02-17 17:49:00 +1000

    added time period parameter

--- increment.py
+++ increment.py
@@ -1,16 +1,14 @@
-def incrementtotalreturn(moneyin, payinterval, brokerage, interest):
-    return incrementpartsreturn(moneyin//(365//payinterval), payinterval, brokerage, interest)
+def incrementtotalreturn(moneyin, payinterval, brokerage, interest, timeinterval):
+    return incrementpartsreturn(moneyin//(365//payinterval), payinterval, brokerage, interest, timeinterval)
 
-def incrementpartsreturn(pay, interval, brokerage, interest):
+def incrementpartsreturn(pay, payinterval, brokerage, interest, timeinterval):
     daily = ((interest + 1) ** (1/365)) # - 1
     initialamount = 0
     count = 0
     
-    for day in range(365):
-        if day % interval == 0 and day != 0:
+    for day in range(timeinterval):
+        if day % payinterval == 0 and day != 0:
             initialamount += (pay - brokerage)
             count += 1
         initialamount *= daily
     return initialamount
-
-print(incrementtotalreturn(13000, 14, 10, 0.07))
