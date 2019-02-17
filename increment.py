488487181d9cd74d1d7085db0a803701d10da95a
Jack Mason <jack.mason@posteo.net>
2019-02-17 17:10:23 +1000

    added increment function based on a total amount

--- increment.py
+++ increment.py
@@ -1,10 +1,16 @@
-def incrementreturn(fortnight, brokerage, interest):
+def incrementtotalreturn(moneyin, payinterval, brokerage, interest):
+    return incrementpartsreturn(moneyin//(365//payinterval), payinterval, brokerage, interest)
+
+def incrementpartsreturn(pay, interval, brokerage, interest):
     daily = ((interest + 1) ** (1/365)) # - 1
     initialamount = 0
+    count = 0
     
     for day in range(365):
-        if day % 14 == 0:
-            initialamount += (fortnight - brokerage)
-            print(initialamount)
+        if day % interval == 0 and day != 0:
+            initialamount += (pay - brokerage)
+            count += 1
         initialamount *= daily
     return initialamount
+
+print(incrementtotalreturn(13000, 14, 10, 0.07))
